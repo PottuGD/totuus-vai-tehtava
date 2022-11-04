1ed13de8408b6c4ca46192acfe56394bfb2ea852
PottuGD <83907925+PottuGD@users.noreply.github.com>
2022-11-04 15:20:13 +0200

    Update choices.py

--- choices.py
+++ choices.py
@@ -1,7 +1,30 @@
 truths = [
-    "Onko sinulla ihastusta?"
+    "Onko sinulla ihastusta?",
+    "Mikä on pahin tapasi?",
+    "Onko sinulla salaista ihastusta?",
+    "Oletko koskaan valedellut, miksi olet myöhässä koulusta?",
+    "Mikä on pahin ateria, mitä äitisi tai isäsi on keittiössä valmistanut?",
+    "Haluaisitko mielummin lemmikin vai sisaruksen?",
+    "Oletko koskaan pissannut sänkyyn tai uima-altaaseen?",
+    "Kuka julkkis haluaisit olla ja miksi?",
+    "Oletko koskaan itkenyt elokuvan aikana?",
+    "Luistatko koskaan kotitöistä, ja jos luistat, mikä on tekosyysi?",
+    "Oletko koskaan lintsannut?",
+    "Mikä haluat olla isona?",
+    "Pelkäätkö pimeää?",
+    "Mikä on pelottavin näkemäsi painajainen?",
+    "Minkä yhden asian muuttaisit itsessäsi?",
+    "Jos tapaisit lampunhengen, mitkä olisivat toivomasi kolme asiaa?"
 ]
 
 dares = [
-    "Lyö itseäsi 5 kertaa."
+    "Lyö itseäsi 5 kertaa. (älä kovaa)",
+    "Älä räpytä silmiäsi minuuttiin.",
+    "Mene ulos ja huuda 'Kaivan nenää' ensimmäiselle ihmiselle, johon törmäät.",
+    "Aseta jalkasi pääsi taakse.",
+    "Mene ulos ja laula _Let It Go_ parhaalla oopperaäänelläsi!",
+    "Anna jonkun kaverisi meikata sinut hullun näköiseksi.",
+    "Tee haarahyppyjä, 2 minuuttia.",
+    "Pyörähdä ympäri kymmenen kertaa ja yritä sen jälkeen kävellä suoraan.",
+    "Mene koputtamaan jonkun oveen ja kysy jotain, esim. Missä on vessa?"
 ]
